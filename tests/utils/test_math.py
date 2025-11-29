a3022bc900ac857075757757e370828d7755e4ee
Jakub Janeczko <kuba@ev1.pl>
2025-11-29 23:35:35 +0100

    improve vector normalization tests

--- tests/utils/test_math.py
+++ tests/utils/test_math.py
@@ -11,35 +11,40 @@ from .streams import stream_testbench
 @pytest.mark.parametrize(
     "data, expected",
     [
-        ([1.0, 0.0, 0.0], [1.0, 0.0, 0.0]),
-        ([0.0, 1.0, 0.0], [0.0, 1.0, 0.0]),
-        ([0.0, 0.0, 1.0], [0.0, 0.0, 1.0]),
-        ([3.0, 4.0, 0.0], [0.6, 0.8, 0.0]),
-        ([-3.0, -4.0, 0.0], [-0.6, -0.8, 0.0]),
-        ([-3.0, 4.0, 0.0], [-0.6, 0.8, 0.0]),
-        ([0.2, 0.0, 0.0], [1.0, 0.0, 0.0]),
+        ([[1.0, 0.0, 0.0]], [[1.0, 0.0, 0.0]]),
+        ([[0.0, 1.0, 0.0]], [[0.0, 1.0, 0.0]]),
+        ([[0.0, 0.0, 1.0]], [[0.0, 0.0, 1.0]]),
+        ([[3.0, 4.0, 0.0]], [[0.6, 0.8, 0.0]]),
+        ([[-3.0, -4.0, 0.0]], [[-0.6, -0.8, 0.0]]),
+        ([[-3.0, 4.0, 0.0]], [[-0.6, 0.8, 0.0]]),
+        ([[0.2, 0.0, 0.0]], [[1.0, 0.0, 0.0]]),
     ]
-    + [([float(i), 0.0, 0.0], [1.0, 0.0, 0.0]) for i in range(1, 10)],
+    + [([[float(i), 0.0, 0.0]], [[1.0, 0.0, 0.0]]) for i in range(1, 10)]
+    + [([[0.0, 1.0, 0.0], [1.0, 0.0, 0.0]], [[0.0, 1.0, 0.0], [1.0, 0.0, 0.0]])],
 )
-def test_normalize(data: list[float], expected: list[float]):
-    dut = FixedPointVecNormalize(Vector3, inv_sqrt_steps=6)
+def test_normalize(data: list[list[float]], expected: list[list[float]]):
+    dut = FixedPointVecNormalize(Vector3)
 
     async def output_checker(ctx, results):
         results = [[v.as_float() for v in r] for r in results]
         print("Input data:", data)
         print("Checking output:", results)
-        print("Expected data:", [expected])
+        print("Expected data:", expected)
         print()
 
-        assert len(results) == 1
-        assert all(abs(r - e) < 1e-3 for r, e in zip(results[0], expected))
+        def vec_dist(a, b):
+            assert len(a) == len(b)
+            return sum((x - y) ** 2 for x, y in zip(a, b)) ** 0.5
+
+        assert len(results) == len(expected)
+        assert all(vec_dist(r, e) < 1e-3 for r, e in zip(results, expected))
 
     sim = Simulator(dut)
     sim.add_clock(1e-6)
     stream_testbench(
         sim,
         input_stream=dut.i,
-        input_data=[Vector3.const(data)],
+        input_data=data,
         output_stream=dut.o,
         output_data_checker=output_checker,
         idle_for=100,
